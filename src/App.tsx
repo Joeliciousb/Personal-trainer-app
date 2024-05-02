31f5944278a65ab3b06f06ee2a400db41cfa9685
Joel <joel.borenius@gmail.com>
2024-05-02 11:57:18 +0300

    index element

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
 import { Box } from "@mui/material";
 import Header from "./Components/Layout/Header";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import CustomerPage from "./Components/Customer/CustomerPage";
 import TrainingPage from "./Components/Training/TrainingPage";
-import "./styles.css";
 import CalendarPage from "./Components/Calendar/CalendarPage";
 import TrainingChartPage from "./Components/TrainingCharts/TrainingChartPage";
+import "./styles.css";
 
 const App = () => {
   return (
     <Box>
       <Header />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<CustomerPage />} />
-          <Route path="/customers" element={<CustomerPage />} />
-          <Route path="/trainings" element={<TrainingPage />} />
-          <Route path="/calendar" element={<CalendarPage />} />
-          <Route path="/reports" element={<TrainingChartPage />} />
-        </Routes>
-      </BrowserRouter>
+
+      <Routes>
+        <Route index element={<CustomerPage />} />
+        <Route path="/customers" element={<CustomerPage />} />
+        <Route path="/trainings" element={<TrainingPage />} />
+        <Route path="/calendar" element={<CalendarPage />} />
+        <Route path="/reports" element={<TrainingChartPage />} />
+      </Routes>
     </Box>
   );
 };
